d8e58fc2301076f227e1a8e63e18b07927f9d1b4
Joanne Arboleda <jbarboleda@ucdavis.edu>
2015-02-20 14:50:03 -0800

    Part 3: Using Native Notification

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 var app = {
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -13,22 +8,26 @@ var app = {
 	},
 
     findByName: function() {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
 
-    initialize: function() 
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    initialize: function() {
 		var self = this;
 		this.store = new MemoryStore(function() {
-			self.renderHomeView();
 			self.showAlert('Store Initialized', 'Info');
 		});
 		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	}
+
 };
 
 app.initialize();
